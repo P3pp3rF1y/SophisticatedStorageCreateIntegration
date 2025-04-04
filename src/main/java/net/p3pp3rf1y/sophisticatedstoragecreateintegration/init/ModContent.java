40de73c7b46dcf27087a0b7a335a35442ed2ddb4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-04-04 15:27:31 +0200

    feat: ✨ Added support for placing Sophisticated Storage blocks with the correct wood type and main/accent colors when using Create schematics

--- src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/init/ModContent.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/init/ModContent.java
@@ -3,6 +3,7 @@ package net.p3pp3rf1y.sophisticatedstoragecreateintegration.init;
 import com.simibubi.create.api.behaviour.movement.MovementBehaviour;
 import com.simibubi.create.api.contraption.storage.item.MountedItemStorageType;
 import com.simibubi.create.api.registry.CreateBuiltInRegistries;
+import com.simibubi.create.api.schematic.nbt.SafeNbtWriterRegistry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.api.distmarker.Dist;
@@ -14,11 +15,13 @@ import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
 import net.p3pp3rf1y.sophisticatedstorage.block.StorageBlockBase;
+import net.p3pp3rf1y.sophisticatedstorage.init.ModBlocks;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.SophisticatedStorageCreateIntegration;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.common.MountedLimitedBarrelContainerMenu;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.common.MountedLimitedBarrelSettingsContainerMenu;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.common.MountedStorageContainerMenu;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.common.MountedStorageSettingsContainerMenu;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.schematic.SophisticatedStorageSafeNbtWriter;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.storage.MountedSophisticatedStorageType;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.storage.SophisticatedStorageMovementBehaviour;
 
@@ -60,5 +63,9 @@ public class ModContent {
 					MountedItemStorageType.REGISTRY.register(block, SOPHISTICATED_MOUNTED_STORAGE_TYPE.get());
 					MovementBehaviour.REGISTRY.register(block, SophisticatedStorageMovementBehaviour.INSTANCE);
 				});
+		SafeNbtWriterRegistry.REGISTRY.register(ModBlocks.CHEST_BLOCK_ENTITY_TYPE.get(), SophisticatedStorageSafeNbtWriter.Wooden.INSTANCE);
+		SafeNbtWriterRegistry.REGISTRY.register(ModBlocks.BARREL_BLOCK_ENTITY_TYPE.get(), SophisticatedStorageSafeNbtWriter.Wooden.INSTANCE);
+		SafeNbtWriterRegistry.REGISTRY.register(ModBlocks.LIMITED_BARREL_BLOCK_ENTITY_TYPE.get(), SophisticatedStorageSafeNbtWriter.Wooden.INSTANCE);
+		SafeNbtWriterRegistry.REGISTRY.register(ModBlocks.SHULKER_BOX_BLOCK_ENTITY_TYPE.get(), SophisticatedStorageSafeNbtWriter.INSTANCE);
 	}
 }
---
+++ src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/schematic/SophisticatedStorageSafeNbtWriter.java
@@ -0,0 +1,35 @@
+package net.p3pp3rf1y.sophisticatedstoragecreateintegration.schematic;
+
+import com.simibubi.create.api.schematic.nbt.SafeNbtWriterRegistry;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.p3pp3rf1y.sophisticatedstorage.block.StorageBlockEntity;
+import net.p3pp3rf1y.sophisticatedstorage.block.StorageWrapper;
+import net.p3pp3rf1y.sophisticatedstorage.block.WoodStorageBlockEntity;
+
+public class SophisticatedStorageSafeNbtWriter implements SafeNbtWriterRegistry.SafeNbtWriter {
+	public static final SophisticatedStorageSafeNbtWriter INSTANCE = new SophisticatedStorageSafeNbtWriter();
+	@Override
+	public void writeSafe(BlockEntity be, CompoundTag tag, HolderLookup.Provider registries) {
+		if (be instanceof StorageBlockEntity storageBlockEntity) {
+			CompoundTag storageWrapperTag = new CompoundTag();
+			storageWrapperTag.putInt(StorageWrapper.MAIN_COLOR_TAG, storageBlockEntity.getStorageWrapper().getMainColor());
+			storageWrapperTag.putInt(StorageWrapper.ACCENT_COLOR_TAG, storageBlockEntity.getStorageWrapper().getAccentColor());
+			tag.put(StorageBlockEntity.STORAGE_WRAPPER_TAG, storageWrapperTag);
+		}
+	}
+
+	public static class Wooden extends SophisticatedStorageSafeNbtWriter {
+		public static final Wooden INSTANCE = new Wooden();
+		@Override
+		public void writeSafe(BlockEntity be, CompoundTag tag, HolderLookup.Provider registries) {
+			super.writeSafe(be, tag, registries);
+			if (be instanceof WoodStorageBlockEntity woodStorageBlockEntity) {
+				woodStorageBlockEntity.getWoodType().ifPresent(woodType -> {
+					tag.putString("woodType", woodType.name());
+				});
+			}
+		}
+	}
+}
