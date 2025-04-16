d104fa196d5ea0917a807b09adcd2e94d4609941
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-04-16 17:23:31 +0200

    feat: ✨ Added compatibility with Trash Slot mod - automatically enables trash slot in sophisticated screens - properly sets up areas where trash slot can / cannot be moved - sets up default position of trashslot below the last toolbar slot (or right next to in case it doesn't fit on larger storage guis)

--- src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/init/ModContentClient.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/init/ModContentClient.java
@@ -2,6 +2,7 @@ package net.p3pp3rf1y.sophisticatedstoragecreateintegration.init;
 
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
+import net.p3pp3rf1y.sophisticatedcore.compat.trashslot.TrashSlotScreenRegistry;
 import net.p3pp3rf1y.sophisticatedstorage.client.ClientEventHandler;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.client.MountedLimitedBarrelScreen;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.client.MountedLimitedBarrelSettingsScreen;
@@ -20,5 +21,7 @@ public class ModContentClient {
 		event.register(ModContent.MOUNTED_STORAGE_SETTINGS_CONTAINER_TYPE.get(), MountedStorageSettingsScreen::constructScreen);
 		event.register(ModContent.MOUNTED_LIMITED_BARREL_CONTAINER_TYPE.get(), MountedLimitedBarrelScreen::new);
 		event.register(ModContent.MOUNTED_LIMITED_BARREL_SETTINGS_CONTAINER_TYPE.get(), MountedLimitedBarrelSettingsScreen::new);
+
+		TrashSlotScreenRegistry.registerScreen(MountedStorageScreen.class);
 	}
 }
