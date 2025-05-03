6368c614f56c5c2b24955ef9083b6c7ab25522b9
Salandora <salandora@gmail.com>
2025-05-03 20:49:59 +0200

    feat: Added EMI compatibility

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/compat/emi/EmiCompat.java
@@ -0,0 +1,48 @@
+package net.p3pp3rf1y.sophisticatedstoragecreateintegration.compat.emi;
+
+import dev.emi.emi.api.EmiEntrypoint;
+import dev.emi.emi.api.EmiPlugin;
+import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.widget.Bounds;
+import net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.emi.GridMenuInfo;
+import net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.emi.SettingsGhostDragDropHandler;
+import net.p3pp3rf1y.sophisticatedcore.compat.recipeviewers.emi.StorageGhostDragDropHandler;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.client.MountedStorageScreen;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.client.MountedStorageSettingsScreen;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.init.ModContent;
+
+@SuppressWarnings("unused")
+@EmiEntrypoint
+public class EmiCompat implements EmiPlugin {
+	@Override
+	public void register(EmiRegistry registry) {
+		registerGuiHandlers(registry);
+		registerRecipeHandlers(registry);
+	}
+
+	private void registerGuiHandlers(EmiRegistry registry) {
+		registry.addExclusionArea(MountedStorageScreen.class, (screen, consumer) -> {
+			//noinspection ConstantValue
+			if (screen == null || screen.getUpgradeSettingsControl() == null) {
+				return;
+			}
+			screen.getUpgradeSlotsRectangle().ifPresent(r -> consumer.accept(new Bounds(r.getX(), r.getY(), r.getWidth(), r.getHeight())));
+			screen.getUpgradeSettingsControl().getTabRectangles().forEach(r -> consumer.accept(new Bounds(r.getX(), r.getY(), r.getWidth(), r.getHeight())));
+			screen.getSortButtonsRectangle().ifPresent(r -> consumer.accept(new Bounds(r.getX(), r.getY(), r.getWidth(), r.getHeight())));
+		});
+		registry.addExclusionArea(MountedStorageSettingsScreen.class, (screen, consumer) -> {
+			//noinspection ConstantValue
+			if (screen == null || screen.getSettingsTabControl() == null) { // Due to how Emi collects the exclusion area this can be null
+				return;
+			}
+			screen.getSettingsTabControl().getTabRectangles().forEach(r -> consumer.accept(new Bounds(r.getX(), r.getY(), r.getWidth(), r.getHeight())));
+		});
+
+		registry.addDragDropHandler(MountedStorageScreen.class, new StorageGhostDragDropHandler<>());
+		registry.addDragDropHandler(MountedStorageSettingsScreen.class, new SettingsGhostDragDropHandler<>());
+	}
+
+	private void registerRecipeHandlers(EmiRegistry registry) {
+		registry.addRecipeHandler(ModContent.MOUNTED_STORAGE_CONTAINER_TYPE.get(), GridMenuInfo.crafting());
+	}
+}
