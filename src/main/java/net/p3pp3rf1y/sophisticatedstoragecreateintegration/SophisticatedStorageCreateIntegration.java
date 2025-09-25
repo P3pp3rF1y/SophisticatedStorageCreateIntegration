58846274940173cfc19f50be4b749f301733777f
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-09-25 11:09:50 +0200

    feat: ✨ Unified sort keybind under Sophisticated Core - Sort keybind that previously existed separately in Sophisticated Storage and Sophisticated Backpacks is now consolidated in Sophisticated Core under the "Sophisticated Mods" category. - ! This is a new keybind and thus it will be set to default Middle Click upon update. - Also added compat with Inventory Tweaks to prevent it from sorting additional sophisticated screens.

--- src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/SophisticatedStorageCreateIntegration.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/SophisticatedStorageCreateIntegration.java
@@ -5,6 +5,7 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.init.ModCompat;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.init.ModContent;
 import net.p3pp3rf1y.sophisticatedstoragecreateintegration.network.ModPayloads;
 import org.apache.logging.log4j.LogManager;
@@ -19,6 +20,7 @@ public class SophisticatedStorageCreateIntegration {
 	public SophisticatedStorageCreateIntegration(IEventBus modBus, Dist dist, ModContainer container) {
 		ModContent.registerHandler(modBus);
 		modBus.addListener(ModPayloads::registerPayloads);
+		ModCompat.register();
 	}
 
 	public static ResourceLocation getRL(String regName) {
