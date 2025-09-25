P3pp3rF1y/SophisticatedBackpacksCreateIntegration

feat: ✨ Unified sort keybind under Sophisticated Core - Sort keybind that previously existed separately in Sophisticated Storage and Sophisticated Backpacks is now consolidated in Sophisticated Core under the "Sophisticated Mods" category. - ! This is a new keybind and thus it will be set to default Middle Click upon update. - Also added compat with Inventory Tweaks to prevent it from sorting additional sophisticated screens.

src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/compat/inventorytweaksrefoxed/InventoryTweaksCompat.java
@@ -1,0 +1,25 @@
+package net.p3pp3rf1y.sophisticatedbackpackscreateintegration.compat.inventorytweaksrefoxed;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.InterModComms;
+import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+
+public class InventoryTweaksCompat implements ICompat {
+	@Override
+	public void init(IEventBus modBus) {
+		ICompat.super.init(modBus);
+		modBus.addListener(this::sendImc);
+	}
+
+	private void sendImc(InterModEnqueueEvent evt) {
+		evt.enqueueWork(() -> {
+			InterModComms.sendTo("invtweaks", "blacklist-screen", () -> "net.p3pp3rf1y.sophisticatedbackpackscreateintegration.common.MountedBackpackContainerMenu");
+		});
+	}
+
+	@Override
+	public void setup() {
+		//noop
+	}
+}

src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/compat/inventorytweaksrefoxed/package-info.java
@@ -1,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedbackpackscreateintegration.compat.inventorytweaksrefoxed;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

src/main/java/net/p3pp3rf1y/sophisticatedbackpackscreateintegration/init/ModCompat.java
@@ -1,0 +1,16 @@
+package net.p3pp3rf1y.sophisticatedbackpackscreateintegration.init;
+
+import net.p3pp3rf1y.sophisticatedbackpackscreateintegration.compat.inventorytweaksrefoxed.InventoryTweaksCompat;
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
+
+public class ModCompat {
+	private ModCompat() {
+	}
+
+	public static void register() {
+		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.INVENTORY_TWEAKS), () -> mobBus -> new InventoryTweaksCompat());
+	}
+}
+

P3pp3rF1y/SophisticatedStorageCreateIntegration

src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/compat/inventorytweaksrefoxed/InventoryTweaksCompat.java
@@ -1,0 +1,25 @@
+package net.p3pp3rf1y.sophisticatedstoragecreateintegration.compat.inventorytweaksrefoxed;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.InterModComms;
+import net.neoforged.fml.event.lifecycle.InterModEnqueueEvent;
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+
+public class InventoryTweaksCompat implements ICompat {
+	@Override
+	public void init(IEventBus modBus) {
+		ICompat.super.init(modBus);
+		modBus.addListener(this::sendImc);
+	}
+
+	private void sendImc(InterModEnqueueEvent evt) {
+		evt.enqueueWork(() -> {
+			InterModComms.sendTo("invtweaks", "blacklist-screen", () -> "net.p3pp3rf1y.sophisticatedstoragecreateintegration.common.MountedStorageContainerMenu");
+		});
+	}
+
+	@Override
+	public void setup() {
+		//noop
+	}
+}

src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/compat/inventorytweaksrefoxed/package-info.java
@@ -1,0 +1,8 @@
+// Auto generated package-info by MCP
+
+@ParametersAreNonnullByDefault @MethodsReturnNonnullByDefault
+package net.p3pp3rf1y.sophisticatedstoragecreateintegration.compat.inventorytweaksrefoxed;
+
+import net.minecraft.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;

src/main/java/net/p3pp3rf1y/sophisticatedstoragecreateintegration/init/ModCompat.java
@@ -1,0 +1,15 @@
+package net.p3pp3rf1y.sophisticatedstoragecreateintegration.init;
+
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatInfo;
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
+import net.p3pp3rf1y.sophisticatedcore.compat.CompatRegistry;
+import net.p3pp3rf1y.sophisticatedstoragecreateintegration.compat.inventorytweaksrefoxed.InventoryTweaksCompat;
+
+public class ModCompat {
+	private ModCompat() {
+	}
+
+	public static void register() {
+		CompatRegistry.registerCompat(new CompatInfo(CompatModIds.INVENTORY_TWEAKS), () -> mobBus -> new InventoryTweaksCompat());
+	}
+}